bc35579869e1e5becb16936088327740694d6663
yaaomens <pmmaytee@gmail.com>
2024-11-20 15:06:23 -0500

    added my python file

---
+++ Helloworld.py
@@ -0,0 +1,51 @@
+
+
+#patient = 'John Smith'
+#ge = 20
+#_a_new_patient = True
+#girlfriend = 'Tay'
+#print(patient)
+#if girlfriend == 'Tay':
+#name = input("What is your name ")
+#print ("Hello" + name)
+
+#x = input ("Who is he?")
+#print (x)
+#birth_year = input("Enter your birth year: ")
+#age = 2024 - int(birth_year)
+#print(age)
+
+#birth_year = input("In what year were you born? ")
+#age = 2024 - int(birth_year)
+#print(age)
+
+
+#First = input("First number: ")
+#print(First)
+#Second = input("Second number: ")
+#print(Second)
+#Sum = (float(First) + int(Second))
+
+course = 'Python for beginners'
+print(course.replace('for', 'y'))
+print('Python' in course)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

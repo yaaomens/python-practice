4a52c3ba07540a363222fba782f72937c2c0c738
yaaomens <pmmaytee@gmail.com>
2024-11-20 16:13:13 -0500

    added comparisons with while loop

--- Helloworld.py
+++ Helloworld.py
@@ -30,11 +30,31 @@ course = 'Python for beginners'
 print(course.replace('for', 'y'))
 print('Python' in course)
 
+#numbers = [1, 2, 3, 4, 5]
+#for i in range(len(numbers)):
+    #if numbers[i]% 2 == 0:
+        #print(f"{numbers[i]} is even")
+    #else:
+        #print(f"{numbers[i]} is odd")
 
+p = 10 == 2
+print(p)
 
+price = 10
+if  (price > 10 and price < 20):
+    print("teee")
 
+teacher = input("What is your name?");
+while teacher == "Yaa" or teacher == "Maryam" :
+    print("Hello teacher")
+    break
 
-
+peter = 5
+tay = 3
+coco = 8
+while coco > tay and peter < tay:
+    print("yay!")
+    break
 
 
 
